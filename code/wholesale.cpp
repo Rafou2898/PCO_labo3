96533cf5424435687e282f5cb2c904c55356d931
Massimo Stefani <massimo.cruz25@gmail.com>
2023-11-04 14:31:04 +0100

    Adding factory

--- code/wholesale.cpp
+++ code/wholesale.cpp
@@ -35,14 +35,14 @@ void Wholesale::buyResources() {
     int qty = rand() % 5 + 1;
     int price = qty * getCostPerUnit(i);
 
-    interface->consoleAppendText(uniqueId, QString("I would like to buy %1 of ").arg(qty) %
+    interface->consoleAppendText(uniqueId, QString("Wholesaler would like to buy %1 of ").arg(qty) %
                                  getItemName(i) % QString(" which would cost me %1").arg(price));
-    if (money <= price && s->trade(i, qty) == price) {
+    if (price <= money && (s->trade(i, qty) == price)) {
         mutex.lock();
         stocks[i] += qty;
         money -= price;
         mutex.unlock();
-        interface->consoleAppendText(uniqueId, QString("I bought %1 of ").arg(qty) %
+        interface->consoleAppendText(uniqueId, QString("Wholesaler bought %1 of ").arg(qty) %
                                      getItemName(i) % QString(" for %1").arg(price));
     }
 }
@@ -73,24 +73,24 @@ std::map<ItemType, int> Wholesale::getItemsForSale() {
 
 
 int Wholesale::trade(ItemType it, int qty) {
-    // TODO: Can be done cleaner with dynamic resolution -> getInerface must be done
+    mutex.lock();
     if (conditionToTrade(it, qty)) {
+        mutex.unlock();
         return 0;
     }
-    mutex.lock();
-    stocks.at(it) -= qty;
+    stocks[it] -= qty;
     int cost = qty * getCostPerUnit(it);
     money += cost;
     mutex.unlock();
     interface->updateFund(uniqueId, money);
     interface->updateStock(uniqueId, &stocks);
+    interface->consoleAppendText(uniqueId, QString("Wholesaler sold %1 of ").arg(getUniqueId(), qty) %
+                                 getItemName(it) % QString(" for %1").arg(cost));
     return cost;
 }
 
 bool Wholesale::conditionToTrade(ItemType it, int qty) {
-    return qty <= 0
-    || getItemsForSale().find(it) == getItemsForSale().end() 
-    || getItemsForSale().at(it) < qty;
+    return qty <= 0 || stocks[it] < qty;
 }
 
 void Wholesale::setInterface(WindowInterface *windowInterface) {
